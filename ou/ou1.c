341cf28afc157171ef97e5cac936eb8d60f5af67
Samuel <samuel04mc@gmail.com>
2023-10-05 20:16:21 +0200

    ou1 fixed

--- ou/ou1.c
+++ ou/ou1.c
@@ -1,19 +1,21 @@
-//Samuel Oskarsson, id23son
+//Name: Samuel Oskarsson
+//CS-id: id23son
+//Task: ou1
 #include <stdio.h>
 #include <stdbool.h>
 int main(void)
 {
 	int choice;
 	bool repeat = true; //set to true so that the loop gets entered
-	float rate = 1; //the exchange rate
-	float price = 1;
-	float sum, sumSEK; //the sum of all the prices and the prices converted to SEK
+	double rate = 1; //the exchange rate
+	double price = 1;
+	double sum, sumSEK; //the sum of all the prices and the prices converted to SEK
 	
 	printf("Your shopping assistant\n");
 	
-	while (repeat == true) //repeats while the user does not press "3"
+	while (repeat == true) //program runs until the user chooses to quit
 	{
-		printf("\n1. Set exchange rate in SEK (current rate: %.2f)\n", rate);
+		printf("\n1. Set exchange rate in SEK (current rate: %.2lf)\n", rate);
 		printf("2. Read prices in the foreign currency\n");
 		printf("3. End\n");
 
@@ -23,17 +25,17 @@ int main(void)
 		{
 		case 1: //set exchange rate
 			printf("\nEnter exchange rate: ");
-			scanf("%f", &rate);
+			scanf("%lf", &rate);
 			repeat = true;
 			break;
 
 		case 2: //read foreign currency and transform into SEK
 			printf("\n");
-			while (price >= 0) //as long as the price is not negative, continue to ask for more prices
+			while (price >= 0)
 			{
 				printf("Enter price (finish with < 0): ");
-				scanf("%f", &price);
-				if (price < 0) //if the price is below zero, jump out of the cycle of asking for prices
+				scanf("%lf", &price);
+				if (price < 0) //price is not allowed to be negative
 				{
 					break;
 				}
@@ -42,8 +44,8 @@ int main(void)
 			}
 
 			sumSEK = sum * rate;
-			printf("\nSum in foreign currency: %.2f\n", sum);
-			printf("Sum in SEK: %.2f\n", sumSEK);
+			printf("\nSum in foreign currency: %.2lf\n", sum);
+			printf("Sum in SEK: %.2lf\n", sumSEK);
 			price = 1;
 			sum = 0;
 			sumSEK = 0;
