3de53ee2371c207afe05298161b657311823deed
Samuel <samuel04mc@gmail.com>
2023-10-10 12:13:44 +0200

    ou2 complete

--- ou/ou2.c
+++ ou/ou2.c
@@ -1,15 +1,16 @@
 //Name: Samuel Oskarsson
 //CS-id: id23son
 //Task: ou2
-//Date: 6th October 2023
-//Version: 4.0
+//Date: 10th October 2023
+//Version: 7.0
 //Description;
 //	A program to calculate the max, min and avg score from an amount of judges
 //	The scores and how many judges there are is for the user to decide
 #include <stdio.h>
 
-//prints start information
-void printInfo(void) { 
+//prints program information
+void print_start_info(void)
+{ 
 	printf("Program information\n"
 	"The program reads in the number of judges and the score from each judge.\n"
 	"Then it calculates the average score without regard to the lowest and\n"
@@ -17,23 +18,23 @@ void printInfo(void) {
 	"lowest and the final average score).\n\n");
 }
 
-//gets a number of judges from the user input and returns it
-//DATA IN: the number of judges given by the user
-//DATA OUT: the number of judges within the given span
-int getJudges(void) { 
-	int nJudge;
+//gets the amount of judges from the user input
+//DATA OUT: the number of judges
+int get_judges(void)
+{ 
+	int judges;
 	do {
 		printf("Number of judges (min 3 and max 10 judges)? ");
-		scanf("%d", &nJudge);
-	} while (!(nJudge >= 3 && nJudge <= 10));
+		scanf("%d", &judges);
+	} while (!(judges >= 3 && judges <= 10));
 	printf("\n");
-	return nJudge;
+	return judges;
 }
 
-//gets the scores from the user related to each of the judges
+//prints the score from each judge
 //DATA IN: the number of judges and the array of scores
-//DATA OUT: prints the judges and asks for user input on their score
-void readScores(int judges, double scores[]) { 
+void read_scores(int judges, double scores[])
+{ 
 	for (int i = 0; i < judges; i++) {
 		printf("Score from judge %d? ", i + 1);
 		scanf("%lf", &scores[i]);
@@ -42,69 +43,64 @@ void readScores(int judges, double scores[]) {
 
 //prints the scores that the user gave as input
 //DATA IN: the number of judges and the array of scores
-//DATA OUT: the score and which judge gave the score
-void loadedScores(int judges, double scores[]) {
+void print_loaded_scores(int judges, double scores[])
+{
 	printf("\nLoaded scores:\n");
-	for (int i = 0; i < judges; i++)
-	{
+	for (int i = 0; i < judges; i++) {
 		printf("Judge %d: %.1lf\n", i + 1, scores[i]);
 	}
 	
 }
 
-//gets the min and max value of the scores and prints them
-//DATA IN: how many judges and the array of scores they gave
-//DATA OUT: the highest and lowest score given by the judges
-void getScores(int n, double scores[], double *minScore, double *maxScore, double *avg) {
+//gets the scores and calculates the min, max and avg scores
+//DATA IN: number of judges, the array of scores, the min, max, and avg scores
+void calc_scores(int judges, double scores[], double *min_score, double *max_score, double *avg)
+{
 	int i;
 	double sum = 0;
-	double minMaxSum = 0;
-	*minScore = scores[0];
-	*maxScore = scores[0];
+	double min_max_sum = 0;
+	*min_score = scores[0];
+	*max_score = scores[0];
 	
-	for ( i = 0; i < n; i++)
-	{
-		if (scores[i] < *minScore)
-		{
-			*minScore = scores[i];
+	for ( i = 0; i < judges; i++) {
+		if (scores[i] < *min_score) {
+			*min_score = scores[i];
 		}
 
-		if (scores[i] > *maxScore)
-		{
-			*maxScore = scores[i];
+		if (scores[i] > *max_score) {
+			*max_score = scores[i];
 		}
-	}
-	for (int i = 0; i < n; i++)
-	{
 		sum = sum + scores[i];
 	}
-	minMaxSum = *maxScore + *minScore;
-	sum = sum - minMaxSum;
-	*avg = sum / (n-2);
+	min_max_sum = *max_score + *min_score;
+	sum = sum - min_max_sum;
+	*avg = sum / (judges-2);
 }
 
-//prints the max, min and avg scores "returned" from getScores
-void printScores(double maxScore, double minScore, double avg) {
+//prints the max, min and avg scores
+//DATA IN: the min, max and avg scores
+void print_scores(double min_score, double max_score, double avg)
+{
 	printf("\nFinal result:\n");
-	printf("Highest judge score: %.1lf\n", maxScore);
-	printf("Lowest judge score: %.1lf\n", minScore);
+	printf("Highest judge score: %.1lf\n", max_score);
+	printf("Lowest judge score: %.1lf\n", min_score);
 	printf("Final average score: %.1lf\n", avg);
 }
 
-//the main function where the functions are run
-int main(void) {
+int main(void)
+{
 	int judges;
 	double min = 0;
 	double max = 0;
 	double avg = 0;
 	
-	printInfo();
+	print_start_info();
 
-	judges = getJudges();
+	judges = get_judges();
 	double scores[judges];
-	readScores(judges, scores);
-	loadedScores(judges, scores);
-	getScores(judges, scores, &min, &max, &avg);
-	printScores(max, min, avg);
+	read_scores(judges, scores);
+	print_loaded_scores(judges, scores);
+	calc_scores(judges, scores, &min, &max, &avg);
+	print_scores(min, max, avg);
 	return 0;
 }
